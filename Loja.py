2de4c6d3ec69f6f5ff6b6ceb998169351dff27d1
haraldyy <69537970+haraldyy@users.noreply.github.com>
2020-08-21 17:39:57 -0300

    Add files via upload

---
+++ Loja.py
@@ -0,0 +1,16 @@
+def pacd():
+    precodaroupa =int(input(''))
+    precoavistad = precodaroupa * 0.09
+    precoavista=precodaroupa - precoavistad
+    precocincox = precodaroupa /5
+    ptotal = (precodaroupa+(precodaroupa*0.17))/10
+    print(f'{"%.2f" % precoavista}')
+    print(f'{"%.2f" % precocincox}')
+    print(f'{"%.2f" % ptotal}')
+
+def main():
+    pacd()
+
+if __name__=='__main__':
+    main()
+    
